1dbeb35816d436c87ce88040a89c0fa664f096b7
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-10 15:58:16 -0400

    initial commit of working countdown timer to random date

---
+++ scripts.js
@@ -0,0 +1,43 @@
+// console.log('sanity check')
+
+function Timer(id, endtime) {
+	this.id = id;
+	this.endtime = endtime;
+	var clock = document.getElementById(id);
+	this.weeksSpan = clock.querySelector('.weeks');
+	this.daysSpan = clock.querySelector('.days');
+	this.hoursSpan = clock.querySelector('.hours');
+	this.minutesSpan = clock.querySelector('.minutes');
+	this.secondsSpan = clock.querySelector('.seconds');
+}
+
+Timer.prototype.getTimeRemaining = function(){
+	var t = Date.parse(this.endtime) - Date.parse(new Date());
+	this.seconds = Math.floor((t / 1000) % 60);
+	this.minutes = Math.floor((t / 1000 / 60) % 60);
+	this.hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	this.days = Math.floor((t / (1000 * 60 * 60 * 24)) % 7);
+	this.weeks = Math.floor(t / (1000 * 60 * 60 * 24 * 7));
+}
+
+Timer.prototype.updateTimer = function(){
+	this.getTimeRemaining();
+	this.weeksSpan.innerHTML = this.weeks;
+	this.daysSpan.innerHTML = this.days;
+	this.hoursSpan.innerHTML = this.hours;
+	this.minutesSpan.innerHTML = this.minutes;
+	this.secondsSpan.innerHTML = this.seconds;
+
+}
+
+
+
+var endDate = new Date('October 20, 2017');
+// console.log(endDate);
+
+var disneyChristmasTimer = new Timer('timer-div', endDate);
+setInterval(
+	function(){
+		disneyChristmasTimer.updateTimer();
+	}, 1000
+	)
